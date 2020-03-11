f9e9679512a6a2b55e9085742858eda23bffa2c0
Andrew Comtois <629222@pdsb.net>
2020-03-11 08:55:15 -0400

    Intro to App Development with Swift (Book 1)

---
+++ 2 Naming and Indentifiers/Naming.playground/Pages/Pet Problem.xcplaygroundpage/Contents.swift
@@ -0,0 +1,29 @@
+// Number Of Dogs
+5
+
+// Number Of Cats
+4
+
+// Number Of Turtles
+3
+//: Then you could copy those numbers onto new lines of code and let the playground do some math for you, like this:
+// Total Number Of Animals
+5 + 4 + 3
+
+// Total Number Of Mammals
+5 + 4
+//: - experiment: As the week goes on, you get more information about what pets will be in the show.\
+//:Change the code above to update the totals.
+//:
+//: For example, if another person is bringing a dog, change the `Number Of Dogs` from `5` to `6` and change the `5` to a `6` everywhere else it appears (`Total Number Of Animals` and `Total Number Of Mammals`).
+//:
+//: If someone is bringing a new kind of animal, use the same pattern as the other animal counts. Add a new comment (`// Number Of Hamsters` for example) on one line with a number on the next line. Don’t forget to update the totals.
+//:
+//: - Another person is bringing a dog.
+//: - One of the turtles is sick and won’t be coming.
+//: - Another person is bringing a cat.
+//: - Someone is bringing a hamster (a mammal).
+//:
+//:  Could there be a better way to solve this problem? Think about it for a minute or two, then move on.
+//:
+//:[Previous](@previous)  |  page 2 of 14  |  [Next: Pet Problem Recap](@next)
---
+++ 8 Constants and Variables/Variables.playground/Pages/Declaring Variables.xcplaygroundpage/Contents.swift
@@ -0,0 +1,32 @@
+
+// Declaring a constant
+let placeOfBirth = "New Jersey"
+
+// Declaring a variable
+var currentLocation = "New Jersey"
+
+/*:
+ Once you have declared a variable, you can assign a new value to it:
+*/
+// Assigning a new value to a variable
+currentLocation = "California"
+
+/*:
+ - experiment: Below, add a line of code that assigns a new value to `currentLocation`:
+ */
+// Assign a new value
+currentLocation = "obama"
+print(currentLocation)
+/*:
+ In most ways, constants and variables are the same:
+- Both use the same rules for names
+- Both associate a name with an assigned value
+- Both have a specific type
+ 
+ The difference is:
+ - The value of a constant *cannot* be changed after it is first assigned
+ - The value of a variable *can* be changed after it is first assigned
+
+Next, learn how to work with variables.
+ */
+//: [Previous](@previous)  |  page 3 of 13  |  [Next: Working with Variables](@next)
